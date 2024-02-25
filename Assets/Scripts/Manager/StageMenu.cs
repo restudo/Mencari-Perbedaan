d8359d0451bfb13d87cfbcdcf1f292dd748ef414
rokuta <restudooctavriandi@gmail.com>
2024-02-26 01:07:19 +0700

    Add stage assets and fix NaN canvas bug

--- Assets/Scripts/Manager/StageMenu.cs
+++ Assets/Scripts/Manager/StageMenu.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Linq;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -6,17 +7,28 @@ using UnityEngine.UI;
 
 public class StageMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject startButton;
     [SerializeField] private GameObject backButton;
 
+    [SerializeField] private GameObject[] startButtons;
+    [SerializeField] private GameObject[] selectedVFX;
     [SerializeField] private Button[] levelButtons;
 
     private int level;
     private int previousLevel;
+    private GameObject currentSelectedLevel;
 
     private void Start()
     {
-        startButton.SetActive(false);
+        level = 1;
+
+        foreach (var button in startButtons)
+        {
+            button.SetActive(false);
+        }
+        foreach (var vfx in selectedVFX)
+        {
+            vfx.SetActive(false);
+        }
 
         int unlockedLevel = GameManager.instance.LoadUnlockedLevel();
         for (int i = 0; i < levelButtons.Length; i++)
@@ -24,12 +36,12 @@ public class StageMenu : MonoBehaviour
             if (i + 1 > unlockedLevel)
             {
                 levelButtons[i].interactable = false;
-                levelButtons[i].transform.GetChild(0).gameObject.SetActive(true); // set locked icon to true
+                levelButtons[i].transform.GetChild(levelButtons[i].transform.childCount - 1).gameObject.SetActive(true); // set locked icon to true, locked icon in the last child
             }
             else
             {
                 levelButtons[i].interactable = true;
-                levelButtons[i].transform.GetChild(0).gameObject.SetActive(false); // set locked icon to false
+                levelButtons[i].transform.GetChild(levelButtons[i].transform.childCount - 1).gameObject.SetActive(false); // set locked icon to false, locked icon in the last child
             }
         }
 
@@ -56,41 +68,50 @@ public class StageMenu : MonoBehaviour
     // assign to every level button
     public void SelectLevel()
     {
+        Debug.Log(level);
+        currentSelectedLevel = EventSystem.current.currentSelectedGameObject;
         previousLevel = level; //for determine condition if level != previous level 
-        level = EventSystem.current.currentSelectedGameObject.transform.GetSiblingIndex() + 1;
+        level = currentSelectedLevel.transform.GetSiblingIndex() + 1;
+        Debug.Log(level);
 
-        RectTransform startButtonRect = (RectTransform)startButton.transform;
-        Vector3 originAnchorPos = startButtonRect.anchoredPosition; //define anchored pos from recttransform in inspector
+        // RectTransform startButtonRect = (RectTransform)startButton.transform;
+        // Vector3 originAnchorPos = startButtonRect.anchoredPosition; //define anchored pos from recttransform in inspector
 
-        startButton.transform.SetParent(EventSystem.current.currentSelectedGameObject.transform);
+        // startButton.transform.SetParent(currentSelectedLevel.transform);
 
-        startButtonRect.anchoredPosition = originAnchorPos;
+        // startButtonRect.anchoredPosition = originAnchorPos;
 
-        if (!startButton.activeSelf || previousLevel != level)
-        {
-            startButton.SetActive(true);
-            startButton.transform.localScale = Vector3.zero;
-            startButton.transform.DOScale(1, 0.3f).SetEase(Ease.OutBack);
-        }
+        // if (!startButton.activeSelf || previousLevel != level)
+        // {
+        //     startButton.SetActive(true);
+        //     startButton.transform.localScale = Vector3.zero;
+        //     startButton.transform.DOScale(1, 0.3f).SetEase(Ease.OutBack);
+        // }
+
+        startButtons[previousLevel - 1].SetActive(false);
+        selectedVFX[previousLevel - 1].SetActive(false);
+
+        startButtons[level - 1].SetActive(true);
+        selectedVFX[level - 1].SetActive(true);
     }
 
     // assign to background gameobject
     public void HideStartButton()
     {
-        if (startButton.activeSelf)
-        {
-            startButton.transform.DOScale(0, 0.3f).SetEase(Ease.OutQuart).OnComplete(() =>
-            {
-                startButton.SetActive(false);
-            });
-        }
+        // startButtons[level - 1].transform.DOScale(0, 0.3f).SetEase(Ease.OutQuart).OnComplete(() =>
+        // {
+        //     startButtons[level - 1].SetActive(false);
+        // });
+
+        startButtons[level - 1].SetActive(false);
+        selectedVFX[level - 1].SetActive(false);
     }
 
     public void LoadToLevel()
     {
         DOTween.KillAll();
 
-        SceneController.instance.LoadScene(((Scenes)level).ToString());
+        SceneController.instance.LoadScene(((Scenes)level - 1).ToString());
         GameManager.instance.isGameActive = true;
     }
 
